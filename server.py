f89e447cd662f87fdba7050ef01f8675df8b3966
Aditya Raj Singh <adityarajsingh64@gmail.com>
2022-11-12 02:56:21 +0530

    feat: 🎸 Added CORS functionality

--- server.py
+++ server.py
@@ -1,18 +1,23 @@
 from flask import Flask, jsonify, request
-
+#from flask_cors import cross_origin
+from flask_cors import CORS
 from airplane import airplane_seating
 
 app = Flask(__name__)
 
+CORS(app)
+
 @app.route('/airplane-algo', methods=["POST"])
+#@cross_origin()
 def process():
     content = request.json
     seatsGrid = content["seatsGrid"]
     passengers = content["passengers"]
-    result = {}
     seating_arrangement = airplane_seating(seatsGrid, passengers)
     result = {"arrangement": seating_arrangement}
-    return jsonify(result)
+    response = jsonify(result)
+    response.headers.add("Access-Control-Allow-Origin", "http://127.0.0.1:3000")
+    return response
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
